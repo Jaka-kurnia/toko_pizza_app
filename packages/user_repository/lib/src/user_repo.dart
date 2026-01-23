ec32f1fb7b384f2ee6e2375a5771be5b168fd5a1
Jaka-kurnia <jakakur1993@gmail.com>
2026-01-23 10:34:34 +0700

    Duh lieur

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,11 +1,24 @@
+// Import model MyUser
 import 'models/models.dart';
 
+// Abstract class sebagai kontrak repository user
+// Semua implementasi (Firebase, API, dll) WAJIB mengikuti ini
 abstract class UserRepository {
+
+  // Stream yang mengirimkan data user saat status auth berubah
+  // Digunakan untuk memantau login / logout secara real-time
   Stream<MyUser> get user;
 
+  // Method untuk mendaftarkan user baru
+  // Mengembalikan data MyUser setelah berhasil sign up
   Future<MyUser> signUp(MyUser myUser, String password);
 
+  // Method untuk menyimpan data user ke database (misalnya Firestore)
   Future<void> setUserData(MyUser myUser);
+
+  // Method untuk login menggunakan email dan password
   Future<void> signin(String email, String password);
+
+  // Method untuk logout user dari aplikasi
   Future<void> logOut();
 }
